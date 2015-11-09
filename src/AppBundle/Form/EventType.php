e938fe06d85ec89c0f634b726707ecdd20882f94
zenbook <saytaine@gmail.com>
2015-11-09 15:02:44 +0100

    wip creation formulaire

--- src/AppBundle/Form/EventType.php
+++ src/AppBundle/Form/EventType.php
@@ -5,7 +5,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CalendarType extends AbstractType
+class EventType extends AbstractType
 {
   public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -15,7 +15,7 @@ class CalendarType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\Calendar',
+            'data_class' => 'AppBundle\Entity\Event',
         ));
     }
 }
