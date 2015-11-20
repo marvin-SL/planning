4a0e2508d037c0b2ae741395041384fa0e2b16ad
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2015-11-20 10:01:41 +0100

    WIP changement de structure de fichiers

---
+++ src/AppBundle/Controller/Admin/CalendarController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Calendar;
+use AppBundle\Form\CalendarType;
+
+class CalendarController extends Controller
+{
+
+public function newAction(Request $request)
+{
+  // replace this example code with whatever you need
+  $calendar = new Calendar();
+
+  $form = $this->createForm(new CalendarType(), $calendar);
+
+  $form->handleRequest($request);
+
+  if ($form->isValid())
+  {
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($calendar);
+    $em->flush();
+  }
+
+  return $this->render('AppBundle:Admin/Calendar:new.html.twig', array(
+     'form' => $form->createView(),
+  ));
+
+}
+
+}
