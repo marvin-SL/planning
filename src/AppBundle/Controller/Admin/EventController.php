16491fceffae0b898a68ba2f4454db2c4dc3a327
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2015-11-24 21:56:58 +0100

    enregistrement de la room dans le xml ok. WIP recuperer le nom de la room et non son id

--- src/AppBundle/Controller/Admin/EventController.php
+++ src/AppBundle/Controller/Admin/EventController.php
@@ -35,7 +35,6 @@ class EventController extends Controller
     public function xmlWriterAction(Request $request)
     {
         $data = $request->request->get("data");
-        dump($data);die;
         // var_dump(dirname("web/bundles/app/events.xml"));die;
         file_put_contents("../web/bundles/app/events.xml",$data);
         // $event = new Event();
