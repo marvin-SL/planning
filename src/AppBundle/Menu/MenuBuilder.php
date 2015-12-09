e0565db6052ad43b63e4d971dbd89d0957ff8ab7
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2015-12-09 12:33:25 +0100

    surcharge du template knp_menu, rajout des icones dans le menu

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -8,34 +8,40 @@ use Doctrine\ORM\EntityManager;
 
 class MenuBuilder
 {
-   private $factory;
-   private $em;
+    private $factory;
+    private $em;
 
-   /**
+    /**
     * @param FactoryInterface $factory
     */
-   public function __construct(FactoryInterface $factory, Entitymanager $em)
-   {
-       $this->factory = $factory;
-       $this->em = $em;
-   }
+    public function __construct(FactoryInterface $factory, Entitymanager $em)
+    {
+        $this->factory = $factory;
+        $this->em = $em;
+    }
 
-   /**
+    /**
     * main menu
     *
     * @param Request $request
     *
     * @return FactoryInterface
     */
-   public function createMainMenu(Request $request)
-   {
-       $menu = $this->factory->createItem('root');
+    public function createMainMenu(Request $request)
+    {
+        $menu = $this->factory->createItem('root');
 
-       $menu->addChild('Index', array('route' => 'admin_dashboard',
-                                      'label' => 'Tableau de bord'
-                                  ));
-       // ... ajoutez ici les autres liens de base
+        $menu->addChild('Index', array('route' => 'admin_dashboard',
+        'label' => 'Tableau de bord',
+        'extras'    => array('icon'  => 'fa fa-fw fa-dashboard'),
+        ));
 
-       return $menu;
-   }
+        $menu->addChild('Planning', array('route' => 'admin_calendar_show',
+        'label' => 'Planning',
+        'extras'    => array('icon'  => 'fa fa-fw fa-table'),
+        ));
+    // ... ajoutez ici les autres liens de base
+
+    return $menu;
+    }
 }
