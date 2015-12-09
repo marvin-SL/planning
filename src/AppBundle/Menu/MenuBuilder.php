5c768130fd6cf379c6cdd14f4916159bc055fc23
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2015-12-09 11:48:52 +0100

    creation du menu et test affichnage  dans al vue

--- src/AppBundle/Menu/MenuBuilder.php
+++ src/AppBundle/Menu/MenuBuilder.php
@@ -2,26 +2,38 @@
 
 namespace AppBundle\Menu;
 
-use KnpMenuFactoryInterface;
-use SymfonyComponentHttpFoundationRequest;
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManager;
 
 class MenuBuilder
 {
    private $factory;
+   private $em;
 
    /**
     * @param FactoryInterface $factory
     */
-   public function __construct(FactoryInterface $factory)
+   public function __construct(FactoryInterface $factory, Entitymanager $em)
    {
        $this->factory = $factory;
+       $this->em = $em;
    }
 
+   /**
+    * main menu
+    *
+    * @param Request $request
+    *
+    * @return FactoryInterface
+    */
    public function createMainMenu(Request $request)
    {
        $menu = $this->factory->createItem('root');
 
-       $menu->addChild('Index', array('route' => 'admin_dashboard'));
+       $menu->addChild('Index', array('route' => 'admin_dashboard',
+                                      'label' => 'Tableau de bord'
+                                  ));
        // ... ajoutez ici les autres liens de base
 
        return $menu;
