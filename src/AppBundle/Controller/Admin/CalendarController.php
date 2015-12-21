a79cf2cb5b63cd8dd24070e016710369ca106b14
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2015-12-21 10:59:30 +0100

    fonction serializeToXml réalisée, + nettoyage de la fonction showAction

--- src/AppBundle/Controller/Admin/CalendarController.php
+++ src/AppBundle/Controller/Admin/CalendarController.php
@@ -21,7 +21,6 @@ class CalendarController extends Controller
 
     public function newAction(Request $request)
     {
-        // replace this example code with whatever you need
         $calendar = new Calendar();
 
         $form = $this->createForm(new CalendarType(), $calendar);
@@ -43,40 +42,35 @@ class CalendarController extends Controller
 
     public function showAction(Request $request)
     {
-
-        $teachers = new Teacher();
-        $subjects = new Subject();
-        $classRooms = new ClassRoom();
         $event = new Event();
 
-        $teacherRepository = $this->getDoctrine()
-        ->getRepository('AppBundle:Teacher');
+        $form = $this->createForm(new EventType(), $event);
 
+        $form->handleRequest($request);
+
+        if ($form->isValid())
+        {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($event);
+            $em->flush();
+            $this->SerializeToXmlAction();
+        }
+
+        return $this->render('AppBundle:Admin/Calendar:show.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
+    public function SerializeToXmlAction()
+    {
         $subjectRepository = $this->getDoctrine()
         ->getRepository('AppBundle:Subject');
+        $subjects = $subjectRepository->findAll();
 
-        $classRoomRepository = $this->getDoctrine()
-            ->getRepository('AppBundle:ClassRoom');
         $eventRepository = $this->getDoctrine()
             ->getRepository('AppBundle:Event');
 
-        $classrooms = $classRoomRepository->findAll();
-        $subjects = $subjectRepository->findAll();
-
-        $results = [];
         $tabTeachers = [];
-
-        for($i = 0; $i < sizeof($subjects); $i++){
-            $results['subject'][$i]['subjectName']=$subjects[$i]->getName();
-            foreach ($subjects[$i]->getTeachers() as $teacher)
-            {
-                for($y = 0; $y < sizeof($subjects[$i]); $y++)
-                {
-                    $results['subject'][$i]['firstname'][] = $teacher->getFirstName();
-                    $results['subject'][$i]['lastname'][] = $teacher->getLastName();
-                }
-            }
-        }
 
         for($i = 0; $i < sizeof($subjects); $i++){
             $tabTeachers[]=$subjects[$i]->getName();
@@ -117,30 +111,7 @@ class CalendarController extends Controller
 
         file_put_contents($path,$rootNode->asXML());
 
-        $form = $this->createForm(new EventType(), $event);
-
-        $form->handleRequest($request);
-
-        if ($form->isValid())
-        {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($event);
-            $em->flush();
-        }
-
-
-        return $this->render('AppBundle:Admin/Calendar:show.html.twig', array(
-            'teachers' => $teachers,
-            'subjects' => $subjects,
-            'classRooms' => $classrooms,
-            'results' => $results,
-            'form' => $form->createView(),
-        ));
-    }
-
-    public function SerializeToXmlAction(Calendar $calendrier, Event $event)
-    {
-
+        return array();
     }
 
 }
