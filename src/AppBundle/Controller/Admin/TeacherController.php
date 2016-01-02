b88d18b530a73392eba7cc55c6e125dd4bd28a77
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-02 19:15:34 +0100

    function edition teacher

--- src/AppBundle/Controller/Admin/TeacherController.php
+++ src/AppBundle/Controller/Admin/TeacherController.php
@@ -75,4 +75,80 @@ class TeacherController extends Controller
         ));
 
     }
+
+    public function editAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Teacher')->find($id);
+
+        //$deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new TeacherType(), $entity);
+        $editForm->handleRequest($request);
+
+        $teachers = $em->getRepository('AppBundle:Teacher')->findAll();
+
+        if($editForm->isValid()){
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            $message = $this->get('translator')->trans('teacher.update_success', array(), 'flashes');
+            $this->get('session')->getFlashBag()->add('success', $message);
+
+            return $this->redirect($this->generateUrl('admin_teacher_index'));
+        }
+
+        return $this->render('AppBundle:Admin/Teacher:edit.html.twig', array(
+           'edit_form'   => $editForm->createView(),
+          // 'delete_form' => $deleteForm->createView(),
+           'entity' => $entity,
+           'teachers' => $teachers,
+        ));
+    }
+
+    /**
+     * Deletes a Teacher entity.
+     *
+     * @param Request $request
+     * @param integer $id
+     *
+     * @return Symfony\Component\HttpFoundation\Response
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $form = $this->createDeleteForm($id);
+
+        if ($form->handleRequest($request)->isValid()) {
+
+            if (!$entity = $em->getRepository('AppBundle:Teacher')->find($id)) {
+                throw $this->createNotFoundException('Unable to find Teacher entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+
+            $message = $this->get('translator')->trans('teacher.delete_success', array(), 'flashes');
+            $this->get('session')->getFlashBag()->add('success', $message);
+        }
+
+        return $this->redirect($this->generateUrl('admin_teacher_index'));
+    }
+
+    /**
+     * Creates a form to delete a Teacher entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_teacher_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'button.delete', 'translation_domain' => 'forms'))
+            ->getForm();
+    }
 }
