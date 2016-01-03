734f0b1e0287179d01717653056b5ac933f062f0
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-03 16:22:26 +0100

    travail sur la vue user_index

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
 * @ORM\Entity
@@ -32,6 +33,22 @@ class User extends BaseUser
    * @ORM\Column(name="lastname", type="string", length=255, nullable=true)
    */
   protected $lastname;
+
+  /**
+   * @var \DateTime $createdAt
+   *
+   * @Gedmo\Timestampable(on="create")
+   * @ORM\Column(type="datetime")
+   */
+  private $createdAt;
+
+  /**
+   * @var \DateTime $updatedAt
+   *
+   * @Gedmo\Timestampable(on="update")
+   * @ORM\Column(type="datetime")
+   */
+  private $updatedAt;
 
 
   public function __construct()
@@ -103,5 +120,53 @@ class User extends BaseUser
         }
 
         return $highestRole;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return User
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
     }
 }
