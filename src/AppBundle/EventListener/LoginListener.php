448636264037d7e0fafcef8229a0121e5b7157ff
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-08 12:25:59 +0100

    typo

--- src/AppBundle/EventListener/LoginListener.php
+++ src/AppBundle/EventListener/LoginListener.php
@@ -37,7 +37,7 @@ class LoginListener
 		if ($this->securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
             $user = $event->getAuthenticationToken ()->getUser ();
 
-           if ($user->getPasswordChanged () === null) {
+           if ($user->getPasswordChangedAt () === null) {
 
                $this->dispatcher->addListener ( KernelEvents::RESPONSE, array (
                        $this,
