ad6bcf06fe052dd8fdfebd4f2c490f196617918a
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-11 12:32:44 +0100

    testCreateTeacher Ok

--- src/AppBundle/Tests/Controller/Admin/TeacherControllerTest.php
+++ src/AppBundle/Tests/Controller/Admin/TeacherControllerTest.php
@@ -2,12 +2,19 @@
 
 namespace AppBundle\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Client;
+use AppBundle\Tests\WebTestCase;
 
+/**
+ *  test on teacher
+ */
 class TeacherControllerTest extends WebTestCase
 {
-    public function testIndex()
+    /**
+     * test on createTeacher
+     * @return [type] [description]
+     */
+    public function testCreateTeacher()
     {
         $client = static::createClient();
 
@@ -15,7 +22,7 @@ class TeacherControllerTest extends WebTestCase
 
         $crawler = $client->request('GET', '/admin/teachers/');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/nodes");
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/teachers");
 
         $crawler = $client->click($crawler->selectLink('Ajouter')->link());
 
@@ -28,7 +35,7 @@ class TeacherControllerTest extends WebTestCase
 
         $crawler = $client->followRedirect();
 
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("a bien été créé(e) ")')->count(), 'Missing element html:contains("a bien été créé(e) ")');
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("L\'enseignant(e)")')->count(), 'Missing element html:contains("L\'enseignant(e) / intervenant(e) a bien été créé(e)")');
     }
 
 }
