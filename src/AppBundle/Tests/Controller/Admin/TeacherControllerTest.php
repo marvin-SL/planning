a444dd06007abe32abb3bd40d930cc97a508b81a
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-13 16:40:08 +0100

    testEdit teacher commencté

--- src/AppBundle/Tests/Controller/Admin/TeacherControllerTest.php
+++ src/AppBundle/Tests/Controller/Admin/TeacherControllerTest.php
@@ -17,52 +17,11 @@ use AppBundle\Tests\WebTestCase;
  */
 class TeacherControllerTest extends WebTestCase
 {
-    // /**
-    //  * test on index
-    //  * @return [type] [description]
-    //  */
-    // public function testIndex()
-    // {
-    //     $client = static::createClient();
-    //
-    //     $this->login($client, 'marvin.sainteluce', 'cmw');
-    //
-    //     $crawler = $client->request('GET', '/admin/teachers/');
-    //
-    //     $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/teachers");
-    //
-    //     $this->assertGreaterThan(0, $crawler->filter('html:contains("Julian")')->count(), 'Missing element html:contains("Julian")');
-    // }
-    //
-    // /**
-    //  * test on createTeacher
-    //  * @return [type] [description]
-    //  */
-    // public function testNewTeacher()
-    // {
-    //     $client = static::createClient();
-    //
-    //     $this->login($client, 'marvin.sainteluce', 'cmw');
-    //
-    //     $crawler = $client->request('GET', '/admin/teachers/');
-    //
-    //     $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/teachers");
-    //
-    //     $crawler = $client->click($crawler->selectLink('Ajouter')->link());
-    //
-    //     $form = $crawler->selectButton('Créer')->form(array(
-    //         'lastname'  => 'Foo',
-    //         'firstname'  => 'Bar',
-    //     ));
-    //
-    //     $client->submit($form);
-    //
-    //     $crawler = $client->followRedirect();
-    //
-    //     $this->assertGreaterThan(0, $crawler->filter('html:contains("L\'enseignant(e)")')->count(), 'Missing element html:contains("L\'enseignant(e) / intervenant(e) a bien été créé(e)")');
-    // }
-
-    public function testEditTeacher()
+    /**
+     * test on index
+     * @return [type] [description]
+     */
+    public function testIndex()
     {
         $client = static::createClient();
 
@@ -72,24 +31,57 @@ class TeacherControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/teachers");
 
-        $link = $crawler
-        ->filter('a:contains("Editer")')
-        ->eq(0)
-        ->link()
-        ;
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Julian")')->count(), 'Missing element html:contains("Julian")');
+    }
 
-        $crawler = $client->click($link);
+    /**
+     * test on createTeacher
+     * @return [type] [description]
+     */
+    public function testNewTeacher()
+    {
+        $client = static::createClient();
 
-        $form = $crawler->selectButton('Mettre à jour')->form(array(
-            'lastname'  => 'Foo-edited',
-            'firstname'  => 'Bar-edited',
+        $this->login($client, 'marvin.sainteluce', 'cmw');
+
+        $crawler = $client->request('GET', '/admin/teachers/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/teachers");
+
+        $crawler = $client->click($crawler->selectLink('Ajouter')->link());
+
+        $form = $crawler->selectButton('Créer')->form(array(
+            'lastname'  => 'Foo',
+            'firstname'  => 'Bar',
         ));
 
         $client->submit($form);
 
-        $client->followRedirects();
- file_put_contents('dump2.html',  $client->getResponse()->getContent());
-                $this->assertGreaterThan(0, $crawler->filter('html:contains("L\'enseignant(e) / intervenant(e)")')->count(), 'Missing element html:contains("L\'enseignant(e) / intervenant(e) ")');
+        $crawler = $client->followRedirect();
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("L\'enseignant(e)")')->count(), 'Missing element html:contains("L\'enseignant(e) / intervenant(e) a bien été créé(e)")');
     }
+
+    // public function testEditTeacher()
+    // {
+    //     $client = static::createClient();
+    //
+    //     $this->login($client, 'marvin.sainteluce', 'cmw');
+    //
+    //     $crawler = $client->request('GET', '/admin/teachers/1/edit');
+    //
+    //     $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/teachers/1/edit");
+    //
+    //     $form = $crawler->selectButton('Mettre à jour')->form();
+    //     $form['lastname'] = "Foo-edited";
+    //     $form['firstname'] = "Bar-edited";
+    //
+    //     $client->submit($form);
+    //
+    //     $crawler = $client->followRedirect();
+    //
+    //
+    //             $this->assertGreaterThan(0, $crawler->filter('html:contains("L\'enseignant(e) / intervenant(e)")')->count(), 'Missing element html:contains("L\'enseignant(e) / intervenant(e) ")');
+    // }
 
 }
