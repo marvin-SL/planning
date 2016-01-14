5f81e99706f1630a4f4e9c7c11b8370c5392db22
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-14 16:35:37 +0100

    utilisation de selenium

--- src/AppBundle/Tests/Controller/Admin/ClassroomControllerTest.php
+++ src/AppBundle/Tests/Controller/Admin/ClassroomControllerTest.php
@@ -10,58 +10,78 @@
 */
 namespace AppBundle\Tests\Controller;
 
+use PHPUnit_Extensions_Selenium2TestCase;
 use AppBundle\Tests\WebTestCase;
 
 /**
  *  test on Classroom.
  */
-class ClassroomControllerTest extends WebTestCase
+class ClassroomControllerTest extends PHPUnit_Extensions_Selenium2TestCase
 {
-    /**
-     * test on index.
-     *
-     * @return [type] [description]
-     */
-    public function testIndex()
+    protected function setUp()
     {
-        $client = static::createClient();
-
-        $this->login($client, 'marvin.sainteluce', 'cmw');
-
-        $crawler = $client->request('GET', '/admin/classrooms/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Unexpected HTTP status code for GET /admin/classrooms');
-
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("C343, Copernic")')->count(), 'Missing element html:contains("C343, Copernic")');
+        $this->setBrowser('firefox');
+        $this->setHost('localhost');
+        $this->setPort(4444);
+        $this->setBrowserUrl('http://localhost/planning/');
     }
 
-    /**
-     * test on createClassroom.
-     *
-     * @return [type] [description]
-     */
-    public function testNewClassroom()
+    protected function login()
     {
-        $client = static::createClient();
+        $this->url('http://localhost/planning/login');
+        $form = $this->byCssSelector('form');
+        $action = $form->attribute('action');
+        $this->byName('_username')->value('marvin.sainteluce');
+        $this->byName('_password')->value('cmw');
+        $form->submit();
 
-        $this->login($client, 'marvin.sainteluce', 'cmw');
-
-        $crawler = $client->request('GET', '/admin/classrooms/');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Unexpected HTTP status code for GET /admin/classrooms');
-
-        $crawler = $client->click($crawler->selectLink('Ajouter')->link());
-
-        $form = $crawler->selectButton('Créer')->form(array(
-            'name' => 'Foo',
-        ));
-
-        $client->submit($form);
-
-        $crawler = $client->followRedirect();
-
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("Foo")')->count(), 'Missing element html:contains("Foo")');
     }
+
+    // /**
+    //  * test on index.
+    //  *
+    //  * @return [type] [description]
+    //  */
+    // public function testIndex()
+    // {
+    //     $client = static::createClient();
+    //
+    //     $this->login($client, 'marvin.sainteluce', 'cmw');
+    //
+    //     $crawler = $client->request('GET', '/admin/classrooms/');
+    //
+    //     $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Unexpected HTTP status code for GET /admin/classrooms');
+    //
+    //     $this->assertGreaterThan(0, $crawler->filter('html:contains("C343, Copernic")')->count(), 'Missing element html:contains("C343, Copernic")');
+    // }
+    //
+    // /**
+    //  * test on createClassroom.
+    //  *
+    //  * @return [type] [description]
+    //  */
+    // public function testNewClassroom()
+    // {
+    //     $client = static::createClient();
+    //
+    //     $this->login($client, 'marvin.sainteluce', 'cmw');
+    //
+    //     $crawler = $client->request('GET', '/admin/classrooms/');
+    //
+    //     $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Unexpected HTTP status code for GET /admin/classrooms');
+    //
+    //     $crawler = $client->click($crawler->selectLink('Ajouter')->link());
+    //
+    //     $form = $crawler->selectButton('Créer')->form(array(
+    //         'name' => 'Foo',
+    //     ));
+    //
+    //     $client->submit($form);
+    //
+    //     $crawler = $client->followRedirect();
+    //
+    //     $this->assertGreaterThan(0, $crawler->filter('html:contains("Foo")')->count(), 'Missing element html:contains("Foo")');
+    // }
 
     /**
      * test on edit Classroom.
@@ -70,30 +90,36 @@ class ClassroomControllerTest extends WebTestCase
      */
     public function testEditClassroom()
     {
-        $client = static::createClient();
+        //$client = static::createClient();
 
-        $this->login($client, 'marvin.sainteluce', 'cmw');
+        $this->login();
 
-        $crawler = $client->request('GET', '/admin/classrooms/');
+        //$crawler = $client->request('GET', 'admin/classrooms/1/edit');
+        $this->url('http://localhost/planning/admin/classrooms/1/edit');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Unexpected HTTP status code for GET /admin/classrooms');
+        // $this->assertEquals(200, $client->getResponse()->getStatusCode(), 'Unexpected HTTP status code for GET admin/classrooms/1/edit');
 
-        $link = $crawler
-        ->filter('a:contains("Editer")')
-        ->eq(0)
-        ->link()
-        ;
+        $this->clickOnElement('deleteButton');
+        $this->timeouts()->implicitWait(50000);
+        $this->clickOnElement('form_submit');
+        $message = $this->byXpath('//*[@id="flashes"]');
+        $this->assertRegExp("/bien été/i", $message->text());
+        // $form = $crawler->selectButton('Mettre à jour')->form(array(
+        //     'name' => 'Test', ));
 
-        $crawler = $client->click($crawler->selectLink('Ajouter')->link());
+        // $client->submit($form);
+        // $crawler = $client->followRedirect();
 
-        $form = $crawler->selectButton('Créer')->form(array(
-            'name' => 'FooBar',
-        ));
+        // $this->assertGreaterThan(0, $crawler->filter('html:contains("Foo")')->count(), 'Missing element html:contains("Foo")');
+    }
 
-        $client->submit($form);
-
-        $crawler = $client->followRedirect();
-
-        $this->assertGreaterThan(0, $crawler->filter('html:contains("Foo")')->count(), 'Missing element html:contains("Foo")');
+    public function testDeleteClassroom()
+    {
+        $this->login();
+        $this->url('http://localhost/planning/admin/classrooms/1/edit');
+        $this->clickOnElement('deleteButton');
+        $this->clickOnElement('form_submit');
+        $message = $this->byXpath('//*[@id="flashes"]');
+        $this->assertRegExp("/bien été/i", $message->text());
     }
 }
