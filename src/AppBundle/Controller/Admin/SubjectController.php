eac8bf2d7ef633afc136fbc7ebffa273a8eb8cb2
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-26 20:29:37 +0100

    nettoyage

--- src/AppBundle/Controller/Admin/SubjectController.php
+++ src/AppBundle/Controller/Admin/SubjectController.php
@@ -15,30 +15,6 @@ class SubjectController extends Controller
         $subjects = $em->getRepository('AppBundle:Subject')->findAll();
 
         $session = $request->getSession();
-//
-// $results = [];
-//     foreach ($subjects as $subject) {
-//          $results[]['name'] = $subject->getName();
-//
-//         foreach ($subject->getTeachers() as $teacher){
-//
-//              $results[]['teacher_firstname'] = $teacher->getFirstname();
-//              $results[]['teacher_lastname'] = $teacher->getLastname();
-//         }
-//      }
-
-// dump($subjects);die;
-
-        if ($session->get('introduction') == 'true') {
-            $session->getFlashBag()->add(
-                'notice',
-                ''
-            );
-
-            return $this->render('AppBundle:Admin/Subject:index.html.twig', array(
-                    'subjects' => $subjects,
-                ));
-        }
 
         return $this->render('AppBundle:Admin/Subject:index.html.twig', array(
             'subjects' => $subjects,
@@ -65,18 +41,6 @@ class SubjectController extends Controller
             $this->get('session')->getFlashBag()->add('success', $message);
 
             return $this->redirect($this->generateUrl('admin_subject_index'));
-        }
-
-        if ($session->get('introduction') == 'true') {
-            $session->getFlashBag()->add(
-                'notice',
-                ''
-            );
-            $session->remove('introduction');
-
-            return $this->render('AppBundle:Admin/Subject:new.html.twig', array(
-                'form' => $form->createView(),
-            ));
         }
 
         return $this->render('AppBundle:Admin/Subject:new.html.twig', array(
