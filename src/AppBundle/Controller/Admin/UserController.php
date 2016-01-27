5f2cd4e8a3d0ed539287d0e789be903ae9fb077f
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-27 10:19:39 +0100

    ajout fonction edition et createDeleteForm

--- src/AppBundle/Controller/Admin/UserController.php
+++ src/AppBundle/Controller/Admin/UserController.php
@@ -52,22 +52,52 @@ class UserController extends Controller
         ));
     }
 
-    public function showAction($username)
+    public function editAction(Request $request, $id)
     {
         $em = $this->getDoctrine()->getManager();
 
-        $entity = $em->getRepository('AppBundle:User')->findOneByUsername($username);
-
-        if (!$entity) {
-            throw $this->createNotFoundException('Unable to find User entity.');
+        if (!$entity = $em->getRepository('AppBundle:User')->find($id)) {
+            throw $this->createNotFoundException(sprintf('Unable to find user with id "%s"', $id));
         }
 
-        $breadcrumbs->addRouteItem($entity->getUsername(), 'admin_user_show', array('username' => $username));
-        //$deleteForm = $this->createDeleteForm($username);
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createForm(new UserType($this->container->get('security.context')), $entity);
+        $editForm->handleRequest($request);
 
-        return $this->render('AppBundle:Admin/User:show.html.twig', array(
-            'entity' => $entity,
-            //'delete_form' => $deleteForm->createView(),
+        $users = $em->getRepository('AppBundle:User')->findAll();
+
+        if ($editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            $message = $this->get('translator')->trans('user.update_success', array(), 'flashes');
+            $this->get('session')->getFlashBag()->add('success', $message);
+
+            return $this->redirect($this->generateUrl('admin_user_index'));
+        }
+
+        return $this->render('AppBundle:Admin/User:edit.html.twig', array(
+           'form' => $editForm->createView(),
+           'delete_form' => $deleteForm->createView(),
+           'entity' => $entity,
+           'users' => $users,
         ));
+    }
+
+    /**
+     * Creates a form to delete a User entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('admin_subject_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'button.delete', 'translation_domain' => 'forms'))
+            ->getForm();
     }
 }
