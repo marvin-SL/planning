86be6debf4fd9937c997e8b41658ae83a61dd62b
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-01-31 16:14:22 +0100

    added a test for coverall

--- src/AppBundle/Tests/Controller/Admin/UserControllerTest.php
+++ src/AppBundle/Tests/Controller/Admin/UserControllerTest.php
@@ -73,6 +73,41 @@ class UserControllerTest extends WebTestCase
     }
 
     /**
+     * test on createUser logged as Admin role.
+     *
+     */
+    public function testNewUserAsAdmin()
+    {
+        $client = static::createClient();
+
+        $this->login($client, 'laure.robillard', 'cmw');
+
+        $crawler = $client->request('GET', '/admin/users/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/users/");
+
+        $crawler = $client->click($crawler->selectLink('Ajouter')->link());
+
+        $form = $crawler->selectButton('Créer')->form(array(
+            'appbundle_user[email]' => 'bb@bb.fr',
+            'appbundle_user[lastname]' => 'bar',
+            'appbundle_user[firstname]' => 'foo',
+            'appbundle_user[roles]' => 'ROLE_ADMIN',
+        ));
+
+        $client->submit($form);
+
+        $client->followRedirects();
+
+        $this->assertTrue($client->getResponse()->isRedirect(), 'Redirected to /admin/users/');
+
+        $crawler = $client->request('GET', '/admin/users/');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("bar.foo")')->count(), 'Missing element html:contains("bar.foo")');
+
+    }
+
+    /**
      * test on edit User.
      *
      */
