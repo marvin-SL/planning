24cadca49543235ef1eba9ad45ceb293410ff2cc
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-02-06 12:14:06 +0100

    dashboardController OK

--- src/AppBundle/Controller/Admin/DashboardController.php
+++ src/AppBundle/Controller/Admin/DashboardController.php
@@ -8,19 +8,34 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DashboardController extends Controller
 {
+    /**
+     * Dashboard index
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
     public function indexAction(Request $request)
     {
-        $calendars = $this->getDoctrine()
-        ->getRepository('AppBundle:Calendar')->findBy(array('id' => 'DESC'));
-        $classrooms = $this->getDoctrine()
-        ->getRepository('AppBundle:Classroom')->findAll();
-        $subjects = $this->getDoctrine()
-        ->getRepository('AppBundle:Subject')->findAll();
-        $teachers = $this->getDoctrine()
-        ->getRepository('AppBundle:Teacher')->findAll();
-        $users = $this->getDoctrine()
-        ->getRepository('AppBundle:User')->findAll();
+        $calendars['values'] = $this->getDoctrine()
+        ->getRepository('AppBundle:Calendar')->findBy(array(), array('createdAt' => 'DESC'), 5);
+        $classrooms['values'] = $this->getDoctrine()
+        ->getRepository('AppBundle:Classroom')->findBy(array(), array('createdAt' => 'DESC'), 5);
+        $subjects['values'] = $this->getDoctrine()
+        ->getRepository('AppBundle:Subject')->findBy(array(), array('createdAt' => 'DESC'), 5);
+        $teachers['values'] = $this->getDoctrine()
+        ->getRepository('AppBundle:Teacher')->findBy(array(), array('createdAt' => 'DESC'), 5);
+        $users['values'] = $this->getDoctrine()
+        ->getRepository('AppBundle:User')->findBy(array(), array('createdAt' => 'DESC'), 5);
 
+        $calendars['count'] = $this->getDoctrine()
+        ->getRepository('AppBundle:Calendar')->count();
+        $classrooms['count'] = $this->getDoctrine()
+        ->getRepository('AppBundle:Classroom')->count();
+        $subjects['count'] = $this->getDoctrine()
+        ->getRepository('AppBundle:Subject')->count();
+        $teachers['count'] = $this->getDoctrine()
+        ->getRepository('AppBundle:Teacher')->count();
+        $users['count'] = $this->getDoctrine()
+        ->getRepository('AppBundle:User')->count();
 
         return $this->render('AppBundle:Admin/Dashboard:index.html.twig', array(
             "calendars" => $calendars,
@@ -30,5 +45,4 @@ class DashboardController extends Controller
             "users" => $users,
         ));
     }
-
 }
