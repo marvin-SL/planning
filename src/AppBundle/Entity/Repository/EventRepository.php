6597eea991a584b0516b4f1ed1a44aa7e4d82274
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-02-06 12:13:04 +0100

    created/edited repos to add a count function

--- src/AppBundle/Entity/Repository/EventRepository.php
+++ src/AppBundle/Entity/Repository/EventRepository.php
@@ -6,23 +6,33 @@ use AppBundle\Entity\Calendar;
 use Doctrine\ORM\EntityRepository;
 
 /**
- * EventRepository
- *
+ * EventRepository.
  */
- class EventRepository  extends EntityRepository
- {
-
-     /**
-     * Trouve les events d'un calendrier
+class EventRepository  extends EntityRepository
+{
+    /**
+     * Compte le nombre d'évenement
+     * @return [type] [description]
      */
-     public function findCalendarEvents(Calendar $calendar)
-     {
-         $qb = $this->createQueryBuilder('e');
-         $qb->select('e');
-         $qb->leftJoin('e.calendar', 'c');
-         $qb->where('c = :calendar');
-         $qb->setParameter('calendar', $calendar);
+    public function count()
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb->select('COUNT(c)');
 
-         return $qb->getQuery()->getResult();
-     }
- }
+        return $qb->getQuery()->getSingleScalarResult();
+    }
+
+        /**
+         * Trouve les events d'un calendrier.
+         */
+    public function findCalendarEvents(Calendar $calendar)
+    {
+            $qb = $this->createQueryBuilder('e');
+            $qb->select('e');
+            $qb->leftJoin('e.calendar', 'c');
+            $qb->where('c = :calendar');
+            $qb->setParameter('calendar', $calendar);
+
+            return $qb->getQuery()->getResult();
+    }
+}
