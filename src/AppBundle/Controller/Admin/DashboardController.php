7d4a3e5ae82cdb6c39f2e037eb11cf8945c6753f
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-02-06 11:30:58 +0100

    wip dashboard

--- src/AppBundle/Controller/Admin/DashboardController.php
+++ src/AppBundle/Controller/Admin/DashboardController.php
@@ -11,10 +11,23 @@ class DashboardController extends Controller
     public function indexAction(Request $request)
     {
         $calendars = $this->getDoctrine()
-        ->getRepository('AppBundle:Calendar')->findAll();
+        ->getRepository('AppBundle:Calendar')->findBy(array('id' => 'DESC'));
+        $classrooms = $this->getDoctrine()
+        ->getRepository('AppBundle:Classroom')->findAll();
+        $subjects = $this->getDoctrine()
+        ->getRepository('AppBundle:Subject')->findAll();
+        $teachers = $this->getDoctrine()
+        ->getRepository('AppBundle:Teacher')->findAll();
+        $users = $this->getDoctrine()
+        ->getRepository('AppBundle:User')->findAll();
+
 
         return $this->render('AppBundle:Admin/Dashboard:index.html.twig', array(
             "calendars" => $calendars,
+            "classrooms" => $classrooms,
+            "subjects" => $subjects,
+            "teachers" => $teachers,
+            "users" => $users,
         ));
     }
 
