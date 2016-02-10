b3f1c4776877dedaac935f491f71b8ee6e488a6f
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-02-10 14:42:47 +0100

    added library

--- src/AppBundle/Manager/NotificationManager.php
+++ src/AppBundle/Manager/NotificationManager.php
@@ -2,16 +2,22 @@
 
 namespace AppBundle\Manager;
 
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Templating\EngineInterface;
+
 /**
  * Notification Manager
  */
 class NotificationManager extends BaseManager
 {
+    protected $securityContext;
     protected $em;
     protected $mailer;
     private   $templating;
 
-    public function __construct(SecurityContextInterface $securityContext, EntityManager $em, \Swift_Mailer $mailer, EngineInterface $templating, TranslatorInterface $translator)
+    public function __construct(SecurityContextInterface $securityContext, EntityManager $em, \Swift_Mailer $mailer, EngineInterface $templating)
     {
         $this->em               = $em; // récupération de l'entityManager
         $this->mailer           = $mailer;
@@ -19,7 +25,7 @@ class NotificationManager extends BaseManager
 
     /**
      * send function
-     * 
+     *
      * @param  [type] $subject   [description]
      * @param  [type] $body      [description]
      * @param  [type] $sender    [description]
@@ -28,7 +34,7 @@ class NotificationManager extends BaseManager
      * @param  [type] $cc        [description]
      * @return [type]            [description]
      */
-    public function send($subject, $body, $sender, $recipient, $copy, $cc)
+    public function send($subject, $body, $sender, $recipient, $copy = null, $cc = null)
     {
         $message = \Swift_Message::newInstance()
         ->setSubject($subject)
