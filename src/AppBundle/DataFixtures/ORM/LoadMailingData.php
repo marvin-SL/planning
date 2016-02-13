f5a1990522e63082bf343f85cf6ebed8f0569ff9
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-02-13 15:05:18 +0100

    created fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadMailingData.php
@@ -0,0 +1,27 @@
+<?php
+namespace AppBundle\DataFixtures;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\Mailing;
+
+class LoadMailingData extends AbstractFixture
+{
+    public function saveMailing(ObjectManager $manager, $name, $mails)
+    {
+        $mailing = new Mailing();
+        $mailing->setName($name);
+        $mailing->setMails($mails);
+
+        $manager->persist($mailing);
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $this->saveMailing($manager, "liste de diffusion G1", "test1@aa.fr;test2@aa.fr,test3@aa.fr,marvin.ipsos@gmail.com");
+        $this->saveMailing($manager, "liste de diffusion G2", "test4@aa.fr;test5@aa.fr,test6@aa.fr");
+
+        $manager->flush();
+    }
+}
