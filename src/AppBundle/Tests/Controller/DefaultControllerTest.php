42e59a17a49b3ba50e8e1f8a001d481775570428
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-02-22 22:12:31 +0100

    added a test for exception

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -3,6 +3,10 @@
 namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Bundle\FrameworkBundle\Test\Framework;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -15,5 +19,15 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
         $this->assertGreaterThan(0, $crawler->filter('html:contains("groupe 3")')->count(), 'Missing element html:contains("groupe 1")');
+    }
+
+
+    public function testFail()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/calendar/azerty12121212');
+
+        $this->assertTrue($client->getResponse()->isNotFound());
     }
 }
