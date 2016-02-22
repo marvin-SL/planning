44619895a8137a6e6e7c78a05e2bab330d2efe6b
Marvin Sainte-Luce <marvin.sainte-luce@ipsos.com>
2016-02-22 21:07:31 +0100

    added a test

--- src/AppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AppBundle/Tests/Controller/DefaultControllerTest.php
@@ -1,18 +1,19 @@
 <?php
 
-// namespace AppBundle\Tests\Controller;
-//
-// use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-//
-// class DefaultControllerTest extends WebTestCase
-// {
-//     public function testIndex()
-//     {
-//         $client = static::createClient();
-//
-//         $crawler = $client->request('GET', '/');
-//
-//         $this->assertEquals(200, $client->getResponse()->getStatusCode());
-//         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
-//     }
-// }
+namespace AppBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("groupe 3")')->count(), 'Missing element html:contains("groupe 1")');
+    }
+}
